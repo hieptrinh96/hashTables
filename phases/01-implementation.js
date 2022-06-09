cf8f3fb0bd39c1b022c88f3082845f6231a7a592
Hiep Trinh <xmikecolx@gmail.com>
2022-06-09 14:57:10 -0500

    constructor and insert() solved

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -9,8 +9,9 @@ class KeyValuePair {
 class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
-    // Initialize your buckets here
-    // Your code here
+    this.capacity = numBuckets;
+    this.count = 0;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
@@ -30,7 +31,25 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
 
   insert(key, value) {
-    // Your code here
+    const index = this.hashMod(key);
+    let curr = this.data[index];
+    while (curr && curr.key !== key) {
+      curr = curr.next;
+    }
+    if (curr) {
+      curr.value = value;
+    }
+    else {
+      let newNode = new KeyValuePair(key, value);
+      if (!this.data[index]) {
+        this.data[index] = newNode;
+      }
+      else {
+        newNode.next = this.data[index];
+        this.data[index] = newNode;
+      }
+    }
+    this.count++;
   }
 
 
